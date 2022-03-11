6f7013bb5431eda980fa6a83fa3d9914144aeaa8
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-03-11 18:07:09 +0300

    Добавил тестовый коммент

--- Database/users.py
+++ Database/users.py
@@ -4,6 +4,7 @@ import secrets
 
 import settings
 
+#Класс пользователя
 class User:
     def __init__(self, first_name, last_name, login, password, balance=100):
         self.first_name = first_name
