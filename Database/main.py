941a21e37a1cef6405c7ebeaeefc4262c83dc016
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-03-11 18:52:43 +0300

    тест

--- Database/main.py
+++ Database/main.py
@@ -1,9 +1,12 @@
 import db_interface as dbi
 
 dbi.db_initialize()
-dbi.users.add_user(dbi.users.User('Oleg', 'Zhmelev', 'losdayver', '123'))
+# dbi.users.add_user(dbi.users.User('Oleg', 'Zhmelev', 'losdayver', '123'))
+#
+# print(dbi.users.get_user_by_login('losdayver'))
 
-print(dbi.users.get_user_by_login('losdayver'))
+#dbi.users.add_user(dbi.users.User('Petr', 'Petrov', 'pp', '123123123'))
+print(dbi.users.get_user_by_login('ppp'))
 
 while True:
     dbi.print_current_menu()
